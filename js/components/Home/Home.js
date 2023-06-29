3c118592130516e041f3a8cdb9f6eb649e4fb673
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-06-29 23:20:48 +0200

    Update Home.js

--- js/components/Home/Home.js
+++ js/components/Home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import MovieListing from "../MovieListing/MovieListing";
+import "./Home.scss";
+import movieAPI from "../../common/APIs/MovieAPI";
+import {APIKey} from "../../common/APIs/MovieAPIKey";
 
 const Home = () => {
+
+    useEffect(() => {
+        const movieText = "Harry"
+        const fetchMovies = async () => {
+            const response = await movieAPI
+                .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+                .catch((err) => {
+                console.log("Error: ", err);
+            });
+            console.log("Te response from API: ", response);
+        };
+
+        fetchMovies();
+    }, [])
+
     return (
         <div>
-          Home
+        <div className="banner-img"></div>
+            <MovieListing />
         </div>
     );
 };
