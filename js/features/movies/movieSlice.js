35055f4e8f176d26a2beeb476754d94c0e7b0c00
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-07-05 23:36:52 +0200

    Update movieSlice.js

--- js/features/movies/movieSlice.js
+++ js/features/movies/movieSlice.js
@@ -1,4 +1,22 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import movieAPI from "../../common/APIs/MovieAPI";
+import {APIKey} from "../../common/APIs/MovieAPIKey";
+
+
+const fetchAsyncMovies = createAsyncThunk(
+    "movies/fetchAsyncMovies", 
+    async () => {
+
+        const movieText = "Harry"
+        
+    const response = await movieAPI
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+            console.log("Error: ", err);
+        });
+    // console.log("The response from API: ", response);
+    return response.data;
+})
 
 const initialState = {
     movies: {}
@@ -12,7 +30,16 @@ const movieSlice = createSlice({
             state.movies = payload;
         }
     },
-    // extraReducers: {} [LG]: -- not used
+    extraReducers: {
+        [fetchAsyncMovies.pending]: () => {
+            console.log('Pending');
+        },
+        [fetchAsyncMovies.fulfilled]: (state, {payload}) => {
+            console.log("Fetched successfully!");
+            return {...state, movies: payload }
+        } 
+    }
+        
 
 });
 
